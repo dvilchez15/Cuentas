037e72ce0c96918af031b4f06a15ba0cf076b546
dvilchez15 <dvilchez15@gmail.com>
2026-08-17 09:08:19 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='cuenta-clara-v18';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./cuenta-clara-logo-v18-180.png','./cuenta-clara-logo-v18-192.png','./cuenta-clara-logo-v18-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(k=>k.put(e.request,x));return r}).catch(()=>caches.match('./index.html'))))});
