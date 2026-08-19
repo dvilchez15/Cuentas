22a05e5dfdea14bbd16df0d1eb158101f8ea7e55
dvilchez15 <dvilchez15@gmail.com>
2026-08-19 21:18:19 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='cuenta-clara-v22';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./cuenta-clara-logo-v22-180.png','./cuenta-clara-logo-v22-192.png','./cuenta-clara-logo-v22-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(k=>k.put(e.request,x));return r}).catch(()=>caches.match('./index.html'))))});
